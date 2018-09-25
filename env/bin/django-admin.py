d9af7350395996e8dc474b175d1466e082d04a11
Ahmedsebit <Ahmed.yusuf@andela.com>
2018-09-25 12:30:35 +0300

    ADD enviroment files

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ke-c02wf0tahv2f/Desktop/pythonD1-D2_assesment/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
